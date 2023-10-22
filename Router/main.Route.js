70a2a0deb2db59e658a883e0b297c4b0474aa2f4
romzy234 <cyrilogoh@gmail.com>
2023-10-22 19:32:45 +0100

    Major Fix : trust Me

--- Router/main.Route.js
+++ Router/main.Route.js
@@ -1,49 +1,147 @@
-const task = require('../model/task');
+const task = require("../model/task");
+const user = require("../model/user");
+const jwt = require('jsonwebtoken')
 
-const router = require('express').Router();
+const router = require("express").Router();
+const authGuard = async(req,res, next)=>{
+    try {
+    let token;
+    token = req.cookies.token;
+    if (!token) {
+        res.redirect('/login')
+      }
+        const decoded = jwt.verify(token, 'idontlikebread');
+        req.user = await user.findOne({_id:decoded.user_id})
+        next()
+    } catch (error) {
+        console.log(req.cookies);
+        res.redirect('/login')
+    }
+}
 
-router.get('/login', (req,res,next)=>{
- res.render('login')
-})
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
 
+router.get("/register", (req, res, next) => {
+  res.render("register");
+});
 
-router.get('/create', (req,res,next)=>{
- res.render('create')
-})
+router.post("/login", async (req, res, next) => {
+    const {username, password} = req.body
+    try {
+        const findUser = await user.findOne({username, password})
+        const token = jwt.sign(
+            {
+              user_id: findUser._id
+            },
+            'idontlikebread'
+          );
+          console.log(token);
+        const options = {
+            expires: new Date(
+              Date.now() + 1 * 24 * 60 * 60 * 1000
+            ),
+            httpOnly: true
+          };
+        res
+        .status(200)
+        .cookie('token', token, options).redirect('/home')
+      } catch (error) {
+        console.log(error);
+        res.send('Error In Creation, Either Duplicate Or invalid input')
+      }
+});
 
+router.post("/register",async (req, res, next) => {
+  const {username, password} = req.body
+  try {
+    const newUser = await user.create({username, password})
+    const token = jwt.sign(
+        {
+          user_id: newUser._id
+        },
+        'idontlikebread'
+      );
+    const options = {
+        expires: new Date(
+          Date.now() + 1 * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+      };
+    res
+    .status(201)
+    .cookie('token', token, options).redirect('/home')
+  } catch (error) {
+    console.log(error);
+    res.send('Error In Creation, Either Duplicate Or invalid input')
+  }
+});
 
-router.post('/create',async (req,res,next)=>{
-    const {body} = req.body;
-    if(!body){
-        return res.send('Not a valid Body')
+router.get("/create", authGuard, (req, res, next) => {
+  res.render("create");
+});
+
+router.post("/create", authGuard, async (req, res, next) => {
+  try {
+    const { body } = req.body;
+    if (!body) {
+      return res.send("Not a valid Body");
     }
     await task.create({
-        body,
-        user:'a',
-        done:false
-    })
-    res.redirect('/home')
-})
+      body,
+      user: req.user._id,
+      done: false,
+    });
+    res.redirect("/home");
+  } catch (error) {
+    console.log(error);
+    res.send("An Error Occured");
+  }
+});
 
+router.get("/read/:id", authGuard, async (req, res, next) => {
+  try {
+    const t = await task.findOne({ _id: req.params.id });
+    t.done = true;
+    await t.save();
+    res.redirect("/home");
+  } catch (error) {
+    console.log(error);
+    res.send("Crazy things are happening!!");
+  }
+});
 
-router.get('/read/:id',async (req,res,next)=>{
-try {
-    const t = await task.findOne({_id:req.params.id});
-    data.done = false
-    await data.save();
-    res.redirect('/home')
-} catch (error) {
-    res.send('Crazy things are happening!!')
-}
-})
+router.get("/edit/:id", authGuard, async (req, res, next) => {
+  try {
+    const t = await task.findOne({ _id: req.params.id });
+    res.render("edit", { t });
+  } catch (error) {
+    console.log(error);
+    res.send("Crazy things are happening!!");
+  }
+});
 
+router.post("/edit/:id", authGuard, async (req, res, next) => {
+  try {
+    const t = await task.findOne({ _id: req.params.id });
+    t.body = req.body.body;
+    await t.save();
+    res.redirect("/home");
+  } catch (error) {
+    console.log(error);
+    res.send("Crazy things are happening!!");
+  }
+});
 
-router.get('/home',async (req,res,next)=>{
-    try {
-        const tasks = await task.find({user:'a'});
-        res.render('home', {tasks})
-    } catch (error) {
-        res.send('Crazy things are happening!!')
-    }
-    })
+router.get("/home", authGuard, async (req, res, next) => {
+  try {
+    const tasks = await task.find({ user: req.user._id });
+    res.render("home", { tasks });
+  } catch (error) {
+    console.log(error);
+    res.send("Crazy things are happening!!");
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path')
 const app = express();
+const cookieParser = require('cookie-parser')
 
+app.use(express.json({ limit: '40mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 const PORT = 8000;
 const main = require('./Router/main.Route')
 mongoose.connect(
-    `mongodb+srv://eseimhariagbe:admin@bloggingapi.fkdmsin.mongodb.net/blog?retryWrites=true&w=majority`, {
+    `mongodb+srv://eseimhariagbe:admin@bloggingapi.fkdmsin.mongodb.net/todo?retryWrites=true&w=majority`, {
     useNewUrlParser: true
 }
 );
